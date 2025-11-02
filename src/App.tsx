c0d70b1510a0ed33c0802e8ca4c2777d13ba64f6
AlHalpukov <lkarehcjd@gmail.com>
2025-11-02 03:01:13 +0300

    lesson 007 - JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
-const tracks = [
-  {
-    id: 1,
-    title: "Musicfun soundtrack",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-  },
-  {
-    id: 2,
-    title: "Musicfun soundtrack instrumental",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-  },
-  {
-    id: 3,
-    title: "Musicfun soundtrack instrumental",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-  },
-  {
-    id: 4,
-    title: "Musicfun soundtrack instrumental",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-  },
-];
-
-const Item = ({ id, title, url }) => {
-  return (
-    <li key={id}>
-      <div>{title}</div>
-      <audio controls src={url}></audio>
-    </li>
-  );
-};
-
 function App() {
   return (
-    <>
-      <h1>Musicfun Player</h1>
-      <ul>
-        {tracks.map(({ title, url }) => (
-          <Item title={title} url={url} />
-        ))}
-      </ul>
-    </>
+    <div>
+      {/* JSX элементы должны быть обязательно закрыты */}
+      <input
+        type="text"
+        id="title"
+        maxLength={15}
+        placeholder="search"
+        disabled={true}
+        style={{
+          color: "red",
+          border: "10px solid white",
+          backgroundColor: "grey",
+        }}
+      />
+    </div>
   );
 }
 
